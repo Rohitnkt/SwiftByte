d2f13c7ac6812987579afbf3155a769c867ce10f
rohitdell <panditrohitnaokothi@gmail.com>
2026-08-03 01:20:02 +0530

    Secured database configuration using environment variables

--- src/main/java/com/swiftbyte/util/DBConnection.java
+++ src/main/java/com/swiftbyte/util/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	private static final String URL="jdbc:mysql://localhost:3306/swiftbyte";
-	private static final String USERNAME="root";
-	private static final String PASSWORD="password";
+	private static final String URL = System.getenv("DB_URL");
+	private static final String USERNAME = System.getenv("DB_USERNAME");
+	private static final String PASSWORD = System.getenv("DB_PASSWORD");
 	private static Connection con;
  public static Connection getConnection() {
 	// TODO Auto-generated method stub
